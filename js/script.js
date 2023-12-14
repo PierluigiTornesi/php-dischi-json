4debf772e605482b568f263667138d99ad413f5b
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-12-14 16:59:57 +0100

    add: stile pagina, commenti e logo

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+        //apiUrl
         apiUrl: "index.php",
+        //array che conterrá l'elenco dei dischi
         elencoDischi: [],
     };
   },
   created() {
+    //prendo i dati della mia api e li inserisco dentro l'array che conterrá i dischi
     axios.get(this.apiUrl).then((resp) => {
         this.elencoDischi = resp.data;
         console.log(this.elencoDischi);
     })
   },
   methods: {
+    //funzione per prendere il link del poster del disco
     getImageLink(disco){
         return disco.poster;
     }
